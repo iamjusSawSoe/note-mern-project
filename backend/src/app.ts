40bafafde8294cd240cab2b2cae6f7966be85198
SAW SOE HTUT WIN <sawsoehtutwin78@gmail.com>
2024-03-07 23:30:48 +0630

    complete sessions

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
+import MongoStore from "connect-mongo";
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
+import session from "express-session";
 import createHttpError, { isHttpError } from "http-errors";
 import morgan from "morgan";
 import notesRoutes from "./routes/notes";
 import userRoutes from "./routes/users";
+import env from "./util/validateEnv";
 
 const app = express();
 
@@ -11,8 +14,21 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
-app.use("/api/notes", notesRoutes);
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({ mongoUrl: env.MONGO_CONNECTION_STRING }),
+  })
+);
+
 app.use("/api/users", userRoutes);
+app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
   next(createHttpError(404, "Endpoint not found"));
--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -3,6 +3,25 @@ import { RequestHandler } from "express";
 import createHttpError from "http-errors";
 import UserModel from "../models/user";
 
+export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
+  const authenticatedUserId = req.session.userId;
+
+  try {
+    if (!authenticatedUserId)
+      throw createHttpError(401, "User not authenticated");
+
+    const user = await UserModel.findById(authenticatedUserId)
+      .select("+email")
+      .exec();
+
+    if (!user) throw createHttpError(404, "User not found");
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
 interface SignUpBody {
   username?: string;
   email?: string;
@@ -52,8 +71,54 @@ export const signUp: RequestHandler<
       password: passwordHashed,
     });
 
+    req.session.userId = newUser._id;
+
     res.status(201).json(newUser);
   } catch (error) {
     next(error);
   }
+};
+
+interface LoginBody {
+  username?: string;
+  password?: string;
+}
+
+export const login: RequestHandler<
+  unknown,
+  unknown,
+  LoginBody,
+  unknown
+> = async (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    if (!username || !password) throw createHttpError(400, "Params Missing");
+
+    const user = await UserModel.findOne({ username: username })
+      .select("+password +email")
+      .exec();
+
+    if (!user) throw createHttpError(401, "Invalid credentials");
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) throw createHttpError(401, "Invalid credentials");
+
+    req.session.userId = user._id;
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logout: RequestHandler = async (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+    } else {
+      res.sendStatus(200);
+    }
+  });
 };
